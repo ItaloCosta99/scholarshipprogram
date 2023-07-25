bfd1c8fb28ac450a59e49956b8300263c39c3262
Italo Costa <italocostajobs@gmail.com>
2023-07-25 15:41:31 -0300

    first commit

---
+++ src/main/java/com/compass/scholarshipprogram/ScholarshipprogramApplication.java
@@ -0,0 +1,13 @@
+package com.compass.scholarshipprogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScholarshipprogramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScholarshipprogramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/compass/scholarshipprogram/ScholarshipprogramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compass.scholarshipprogram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScholarshipprogramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
